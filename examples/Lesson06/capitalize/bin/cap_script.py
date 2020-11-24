07e5c553144a31c30461b1171eae72314d6f6e84
Chris Barker <PythonCHB@gmail.com>
2020-11-24 15:37:52 -0800

    updated capitize exampel, with entry_points

--- examples/Lesson06/capitalize/bin/cap_script.py
+++ examples/Lesson06/capitalize/bin/cap_script.py
@@ -4,13 +4,7 @@
 A really simple script just to demonstrate packaging
 """
 
-import sys, os
-from capitalize import capital_mod
 from capitalize import main
-
-import sys
-for p in sys.path:
-    print(p)
 
 if __name__ == "__main__":
     main.main()    
--- examples/Lesson06/capitalize/setup.py
+++ examples/Lesson06/capitalize/setup.py
@@ -3,7 +3,8 @@ from setuptools import setup
 setup(name="capitalize",
       version="0.0.1",
       packages=["capitalize"],
-      scripts=['bin/cap_script.py'],
+      # scripts=['bin/cap_script.py'],
+      entry_points={'console_scripts': ['cap_script=capitalize.main:main']},
       )
 
 
