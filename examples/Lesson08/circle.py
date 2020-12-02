d8456a569a0d2cc3c726128a38016704ef7cae69
Chris Barker <PythonCHB@gmail.com>
2020-12-01 21:20:14 -0800

    fixed area property

--- examples/Lesson08/circle.py
+++ examples/Lesson08/circle.py
@@ -4,16 +4,11 @@ class for a full featured circle
 
 import math
 
-print("import the new one")
 
 class Circle:
 
     def __init__(self, radius):
         self.radius = radius
-
-    @property
-    def area(self):
-        return (self.diameter / 2)**2 * math.pi
 
     @property
     def diameter(self):
@@ -22,6 +17,11 @@ class Circle:
     @diameter.setter
     def diameter(self, diameter):
         self.radius = diameter / 2
+
+    @property
+    def area(self):
+        return self.radius**2 * math.pi
+
 
 
 
